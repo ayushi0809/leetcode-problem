023899f228433a42088448e6e763d30af19b4eb2
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-06-24 23:11:23 +0530

    Time: 23 ms (45.02%), Space: 9 MB (37.57%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -10,10 +10,26 @@ public:
             return dp[i][j]= 0+f(i-1,j-1,s,t,dp);
         return dp[i][j] = 1+min(f(i-1,j,s,t,dp),min(f(i,j-1,s,t,dp),f(i-1,j-1,s,t,dp)));
     }
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2,dp);
+    int minDistance(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            dp[i][0] = i;
+        }
+        for(int i=1;i<=m;i++){
+            dp[0][i] = i;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 0+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1+min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
