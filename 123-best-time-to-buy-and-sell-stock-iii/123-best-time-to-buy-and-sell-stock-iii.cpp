71130f994da87d3bfee6d57ca1e66f4d35b2f8bd
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-06-27 18:28:06 +0530

    Time: 786 ms (29.45%), Space: 132.7 MB (36.41%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    int f(int i,int n, int buy, int t, vector<int>& prices, vector<vector<vector<int>>> &dp){
-        if(i==n || t>=2)
+    int f(int i,int n, int t, vector<int>& prices, vector<vector<int>> &dp){
+        if(i==n || t==4)
             return 0;
         int profit;
-        if(dp[i][buy][t] != -1) return dp[i][buy][t];
-        if(buy){
-            return dp[i][buy][t] = max(-prices[i]+f(i+1,n,0,t,prices,dp), 0+f(i+1,n,1,t,prices,dp));
+        if(dp[i][t] != -1) return dp[i][t];
+        if(t%2==0){
+            return dp[i][t] = max(-prices[i]+f(i+1,n,t+1,prices,dp), 0+f(i+1,n,t,prices,dp));
         }
         else{
-            return dp[i][buy][t] = max(prices[i]+f(i+1,n,1,t+1,prices,dp), 0+f(i+1,n,0,t,prices,dp));
+            return dp[i][t] = max(prices[i]+f(i+1,n,t+1,prices,dp), 0+f(i+1,n,t,prices,dp));
         }
         
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
-        vector<vector<int>>after(2, vector<int>(3,0));
-        vector<vector<int>>curr(2, vector<int>(3,0));
-        for(int i =n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                for(int k =1;k<=2;k++){
-                    if(j){
-                        curr[j][k] = max(-prices[i]+after[0][k], after[1][k]);
-                    }
-                    else{
-                        curr[j][k] = max(prices[i]+after[1][k-1], after[0][k]);
-                    }
-                }
-            }
-            after = curr;
-        }
-        return after[1][2];
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        // vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
+        // vector<vector<int>>after(2, vector<int>(3,0));
+        // vector<vector<int>>curr(2, vector<int>(3,0));
+        // for(int i =n-1;i>=0;i--){
+        //     for(int j=0;j<=1;j++){
+        //         for(int k =1;k<=2;k++){
+        //             if(j){
+        //                 curr[j][k] = max(-prices[i]+after[0][k], after[1][k]);
+        //             }
+        //             else{
+        //                 curr[j][k] = max(prices[i]+after[1][k-1], after[0][k]);
+        //             }
+        //         }
+        //     }
+        //     after = curr;
+        // }
+        // return after[1][2];
+        return f(0,n,0,prices,dp);
     }
 };
