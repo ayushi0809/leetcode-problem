6037d1220a63f326ebf64c48b8a287830013958e
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-06-28 12:20:15 +0530

    Time: 186 ms (35.93%), Space: 13.3 MB (83.00%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,19 +1,41 @@
 class Solution {
-   public:
-    int longestStrChain(vector<string> &words) {
-        unordered_map<string, int> dp;
-        int res = 1;
-        sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() < r.size(); });
-        for (string word : words) {
-            dp[word] = 1;
-            for (int i = 0; i < word.size(); i++) {
-                string prev = word.substr(0, i) + word.substr(i + 1);
-                if (dp.find(prev) != dp.end()) {
-                    dp[word] = dp[prev] + 1;
-                    res = max(res, dp[word]);
-                }
+public:
+    bool compare(string &s1, string &s2){
+         
+        if(s1.size() !=  s2.size()+1)
+            return false;
+        int i=0;
+        int j =0;
+        while(i<s1.size() ){
+            if( j<s2.size() && s1[i]==s2[j]){
+                i++;
+                j++;
             }
+              else{
+                  i++;
+              }  
         }
-        return res;
+        if(i==s1.size() && j==s2.size())
+            return true;
+        return false;
+    }
+    static bool comp(string &s1, string &s2){
+        return s1.size()<s2.size();
+    }
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        int m =1;
+        vector<int>dp(n,1);
+        sort(words.begin(),words.end(),comp);
+        for(int i =0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(compare(words[i],words[j]) && 1+dp[j]>dp[i]){
+                   dp[i] = 1+dp[j];
+                    
+                }
+        }
+            m = max(m,dp[i]);
+        }
+        return m;
     }
 };
