8bc1ee26eb5a4a80afdab309a5c140ecd89ce2c5
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-07-12 14:44:42 +0530

    Time: 737 ms (87.08%), Space: 180 MB (97.83%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,66 +10,21 @@
  */
 class Solution {
 public:
-    int len(ListNode* head){
-        int count =0;
-        while(head != NULL){
-            count++;
-            head = head->next;
-        }
-        return count;
-    }
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* prev1 = NULL;
-        ListNode* curr1 = head;
-         ListNode* prev2 = NULL;
-        ListNode* curr2 = head;
+        if(head==nullptr || head->next == nullptr){
+            return head;
+        }
         ListNode* p = head;
-         int l = len(p);
-        if(head == NULL || k == l-k+1 || head->next == NULL)
-            return head;
-        if(k==1 || k == l){
-            while(curr1->next != NULL){
-                prev1 =curr1;
-                curr1 = curr1->next;
-            }
-            p = head;
-            if(p==prev1){
-                 curr1->next  = p;
-                p->next= NULL;
-            }
-            else{
-            curr1->next  = p->next;
-            prev1->next = p;
-            p->next = NULL;
-            }
-            head = curr1;
-            return head;
-            
+        for(int i=0; i<k-1 && p != NULL; i++){
+            p=p->next;
         }
-        int i =0;
-        while(i<k-1){
-            prev1= curr1;
-            curr1=curr1->next;
-            i++;
+        ListNode* slow = head;
+        ListNode* fast = p;
+        while(fast != nullptr && fast-> next != nullptr){
+            slow= slow->next;
+            fast = fast->next;
         }
-       
-        int j =0;
-        while(j<l-k){
-            prev2= curr2;
-            curr2=curr2->next;
-            j++;
-        }
-        
-        
-        //cout<<curr2->next->val<<endl;
-         prev2->next = curr1;
-         prev1->next = curr2;
-        ListNode* q = curr1->next;
-        curr1->next= curr2->next;
-        curr2->next =q;
-         //prev2->next = curr1;
-        
-        cout<<prev1->val<<curr1->val<<prev2->val<<curr2->val<<endl;
+        swap(p->val, slow->val);
         return head;
     }
 };
