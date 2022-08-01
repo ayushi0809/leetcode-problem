c24fb64149bdd790b7920d31329fd806e57b5fa7
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-08-01 19:47:35 +0530

    Time: 2 ms (50.13%), Space: 5.8 MB (90.91%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    unsigned long long int binomialCoeff(int n, int k)
+{
+    unsigned long long int C[n + 1][k + 1];
+    int i, j;
+ 
+    // Calculate value of Binomial Coefficient
+    // in bottom up manner
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= min(i, k); j++) {
+            // Base Cases
+            if (j == 0 || j == i)
+                C[i][j] = 1;
+ 
+            // Calculate value using previously
+            // stored values
+            else
+                C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+        }
+    }
+ 
+    return C[n][k];
+}
+    int uniquePaths(int m, int n) {
+        return int(binomialCoeff(m+n-2, m-1));
+    }
+};
