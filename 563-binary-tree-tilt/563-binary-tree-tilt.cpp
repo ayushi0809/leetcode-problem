090a0d40204177327ae90b8d033ae9c9384144d2
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-08-06 12:28:26 +0530

    Time: 33 ms (44.49%), Space: 23.7 MB (77.25%) - LeetHub

--- 563-binary-tree-tilt/563-binary-tree-tilt.cpp
+++ 563-binary-tree-tilt/563-binary-tree-tilt.cpp
@@ -11,38 +11,21 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*, pair<int,int>>m;
-    void dfs(TreeNode* root){
+    int s=0;
+    int dfs(TreeNode* root){
         if(root == NULL)
-            return ;
-        else if(!root->left && !root->right){
-            m[root] = {root->val, 0};
-            //return;
-        }
-        else if(!root->left){
-            dfs(root->right);
-            m[root] = {root->val+m[root->right].first, abs(0-m[root->right].first)};
-            
-        }
-        else if(!root->left){
-             dfs(root->left);
-             m[root] = {root->val+m[root->left].first, abs(0-m[root->left].first)};
-        }
-        else{
-        dfs(root->left);
-        dfs(root->right);
+            return 0;
+        int l = dfs(root->left);
+        int r = dfs(root->right);
         
-        m[root] = {m[root->left].first+root->val+m[root->right].first, abs(m[root->left].first - m[root->right].first)};
-    }
+        s = s+abs(l-r);
+        
+        return root->val+l+r;
+       
     }
     int findTilt(TreeNode* root) {
         dfs(root);
-        int s =0;
-        for(auto it = m.begin(); it != m.end(); it++){
-           auto a = it->second;
-            cout<<a.first<<" "<<a.second<<endl;
-            s =s +a.second;
-        }
+        
         return s;
     }
 };
