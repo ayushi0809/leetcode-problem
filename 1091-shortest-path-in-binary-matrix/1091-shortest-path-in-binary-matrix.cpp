60c174aafd0411a0665cc1d2ee2297238d0ef623
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-08-24 18:26:06 +0530

    Time: 1064 ms (5.03%), Space: 76 MB (7.67%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,41 +1,38 @@
 class Solution {
-    bool isOutOfBounds(vector<vector<int>>& grid, int r, int c) {
-        if(r < 0 || r >= grid.size()) return true;
-        if(c < 0 || c >= grid[r].size()) return true;    
-        return false;
-    }
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        vector<vector<int>> dirs = {
-            {-1,-1},    {-1,0},     {-1,1},
-            {0,-1},                 {0,1},
-            {1,-1},     {1,0},      {1,1}
+        int n =grid.size();
+        int m = grid[0].size();
+        if(grid[0][0] == 1){
+            return -1;
+        }
+        
+        queue<pair<pair<int,int>,int>>q;
+        q.push({{0,0},1});
+        grid[0][0] = -1;
+        vector<vector<int>> delta = {
+            {1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}
         };
-        using pii = pair<int,int>;
-        int ROWS = grid.size();
-        if(ROWS == 0) return 0;
-        int COLS = grid[0].size();
-        queue<pii> q;
-        if(grid[0][0] == 0) 
-            q.push({0,0});
-        int steps = 0;
-        while(!q.empty()) {
-            int qSize = q.size();
-            steps++;
-            for(int i = 0; i < qSize; i++) {
-                auto [r, c] = q.front();
-                q.pop();
-                if(r == ROWS - 1 && c == COLS - 1) return steps;
-                for(auto& dir : dirs) {
-                    int dr = dir[0] + r;
-                    int dc = dir[1] + c;
-                    if(isOutOfBounds(grid, dr, dc)) continue;
-                    if(grid[dr][dc] == 1) continue;
-                    grid[dr][dc] = 1;
-                    q.push({dr,dc});
+        
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            cout<<it.first.first <<" "<<it.first.second<<endl;
+            if(it.first.first == n-1 && it.first.second == m-1){
+                return it.second;
+            }
+            for(auto x : delta){
+                int r = it.first.first+x[0];
+                int c = it.first.second+x[1];
+                if(r>=0 && r<n && c>=0 && c<m && grid[r][c] == 0){
+                    grid[r][c] = -1;
+                    q.push({{r,c},it.second+1});
                 }
             }
+            
+            
         }
         return -1;
+        
     }
 };
