b5528d30a34abf67508eaa61cf60ae7abd5eaa64
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-09-29 16:24:26 +0530

    Time: 202 ms (18.15%), Space: 33.5 MB (27.22%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-       
-        queue<pair<pair<int,int>,int>>q;
-        vector<int> delta = {0,1,0,-1,0};
-        int n = mat.size();
-        int m = mat[0].size();
-         vector<vector<bool>>vis(n, vector<bool>(m,false));
-        for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                if(mat[i][j] == 0){
-                    q.push({{i,j},0});
-                    vis[i][j] = true;
-                }
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<bool>>vis(m, vector<bool>(n,false));
+        
+        queue<pair<int, pair<int,int>>>q;
+        
+        for(int i =0; i<m; i++){
+            for(int j =0; j<n; j++){
+                 if(mat[i][j] == 0){
+                     q.push({0,{i,j}});
+                     vis[i][j] = true;
+                 }
             }
         }
-        
+        vector<int>delta = {0,1,0,-1,0};
         while(!q.empty()){
             auto it = q.front();
             q.pop();
-            int r = it.first.first;
-            int c = it.first.second;
-            int count = it.second;
-            mat[r][c] = count;
+            int level = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
+            mat[r][c] = level;
             for(int i =0; i<4; i++){
                 int row = r+delta[i];
                 int col = c+delta[i+1];
-                if(row>=0 && row<n && col>=0 && col<m && vis[row][col] == false){
+                
+                if(col>=0 && col<n && row>=0 && row<m && !vis[row][col]){
                     vis[row][col] = true;
-                    q.push({{row,col}, count+1});
+                    q.push({level+1, {row,col}});
                 }
             }
             
+            
         }
+        
         return mat;
     }
 };
