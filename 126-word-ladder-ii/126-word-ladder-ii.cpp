7f0098ec9574d43a1ffd3e36ce58f64f13815554
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-10-08 18:48:05 +0530

    Time: 33 ms (75.44%), Space: 10.2 MB (36.73%) - LeetHub

---
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    unordered_map<string,int>m;
+    vector<vector<string>>ans;
+    void dfs(string endword,vector<string>v,int level,string beginword){
+        if(endword==beginword){
+            v.push_back(endword);
+            reverse(v.begin(),v.end());
+            ans.push_back(v);
+            return;
+        }
+         v.push_back(endword);
+        for(int i =0; i<endword.size();i++){
+            char o = endword[i];
+            for(char ch = 'a'; ch<='z';ch++){
+                endword[i] = ch;
+                //cout<<endword<<endl;
+                auto it = m.find(endword);
+                if(it != m.end() && it->second==level-1){
+                    dfs(endword,v,level-1,beginword);
+                }
+            }
+            endword[i] = o;
+        }
+    }
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>st(wordList.begin(), wordList.end());
+        queue<pair<string,int>>q;
+        q.push({beginWord,0});
+        st.erase(beginWord);
+        int level=0;
+        while(!q.empty()){
+            string word =q.front().first;
+            level=q.front().second;
+            q.pop();
+            //used.clear();
+            cout<<word<<" "<<level<<endl;
+           m[word]=level;
+            if(word==endWord){
+                break;
+            }
+            for(int i =0; i<word.size(); i++){
+                char ori = word[i];
+                for(char ch = 'a'; ch<='z'; ch++){
+                    word[i] = ch;
+                    if(st.count(word)>0){
+                       q.push({word,level+1});
+                        st.erase(word);
+                    }
+                }
+                word[i] = ori;
+            }
+           // level++;
+            
+        }
+       // cout<<m[wordList[0]]<<endl;
+        for(auto it = m.begin(); it!=m.end(); it++){
+            cout<<it->first<<" "<<it->second<<endl;
+        }
+        
+        dfs(endWord,{},m[endWord],beginWord);
+        return ans;
+    
+        
+    }
+};
