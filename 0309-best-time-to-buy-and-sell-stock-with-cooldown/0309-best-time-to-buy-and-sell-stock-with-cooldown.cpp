52734ef5c4791c29af41c93a7bc6bc06ae502016
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2022-12-23 16:18:28 +0530

    Time: 13 ms (28.12%), Space: 11.9 MB (46.95%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int f(int i, int buy, int n, vector<int>& prices, vector<vector<int>> &dp){
+        if(i>=n)
+            return 0;
+        if(dp[i][buy] != -1) return dp[i][buy];
+        if(buy){
+            return dp[i][buy] = max(-prices[i]+f(i+1,0,n,prices,dp), f(i+1,1, n, prices,dp));
+        }
+        else{
+            return dp[i][buy] = max(prices[i]+f(i+2,1,n,prices,dp),f(i+1,0,n,prices,dp));
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>dp(n+2,vector<int>(2,0));
+      
+        for(int i =n-1;i>=0;i--){
+           
+                    dp[i][1] = max(-prices[i]+dp[i+1][0], dp[i+1][1]);
+                
+               
+                    
+                    dp[i][0] = max(prices[i]+dp[i+2][1] , dp[i+1][0]);
+                    
+                   
+                
+            }
+        
+        return dp[0][1];
+    }
+};
