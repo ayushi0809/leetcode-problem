1296d904f6c4fae3b2a02a861b203b68eb0b097f
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2023-01-11 05:15:11 +0530

    Time: 2 ms (65.60%), Space: 6.3 MB (48.65%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -38,11 +38,66 @@ public:
        
     }
     int numDecodings(string s) {
-        vector<int> dp(s.size()+1,-1);
+        if(s[0]== '0'){
+            return 0;
+        }
+        if(s.size() == 1 && s[0] != '0'){
+            return 1;
+        }
+        vector<int> dp(s.size()+1,0);
+        int n = s.size();
         dp[0]=0;
+        if(s[n-1] !='0'){
+            dp[1] = 1;
+        }
+        string str = "";
+            str = str+s[n-2]+s[n-1];
+       // cout<<str<<endl;
+        if(stoi(str)<=26){
+            cout<<str[0]<<endl;
+            if(str[0] == '0' && str[1] == '0'){
+                dp[2]=0;
+            }
+            if(str[0] !='0' && str[1]=='0'){
+                dp[2] =1;
+            }
+            if(str[0] =='0'){
+                dp[2] =0;
+            }
+            if(str[0] !='0' && str[1] != '0'){
+                dp[2] =2;
+            }
+            
+            
+        }
         
-         int t = numDecod(dp,s);
-         return dp[s.size()];
+        else{
+             if(str[0] !='0' && str[1] != '0'){
+                dp[2] =1;
+            }
+            else{
+                dp[2] =0;
+            }
+            
+        }
+        
+        for(int i =3;i<=n; i++){
+            string st = "";
+               st = st+s[n-i]+s[n-i+1];
+            //cout<<st<<endl;
+           if(s[n-i]== '0')
+               dp[i]=0;
+            else if(stoi(st)<=26){
+                //cout<<dp[i-1]<<" "<<dp[i-2]<<endl;
+                dp[i] = dp[i-1]+dp[i-2];
+            }
+            else{
+                dp[i] = dp[i-1];
+            }
+            
+        }
+        
+         return dp[n];
        
     }
 };
