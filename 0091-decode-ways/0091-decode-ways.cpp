347f001402b5f20b5e861fec6cdbc546f342baa5
ayushi0809 <42436449+ayushi0809@users.noreply.github.com>
2023-01-11 04:04:59 +0530

    Time: 7 ms (22.58%), Space: 7.2 MB (20.00%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int numDecod(vector<int> &dp,string s){
+        int n = s.size();
+         //cout<<s<<" "<<n<<endl;
+        
+        if(s.size()==0)
+            return 0;
+        if(s[0]=='0')
+            return dp[n]=0;
+        if(s.size()==1)
+        {
+            dp[1] =1;
+            return 1;
+        }
+        if(s.size()==2 && stoi(s)<=26 &&stoi(s)>=1)
+        {
+            if(s[1]=='0')
+            {
+                dp[2]=1;
+                return 1;
+            }
+            dp[2]=2;
+            return 2;
+        }
+        int left =0;
+        int right =0;
+        if(dp[n]!=-1){
+            cout<<dp[n]<<endl;
+            return dp[n];
+        }
+        if(s[0]!=0)
+            left = numDecod(dp,s.substr(1));
+        if(s[0] !=0 && stoi(s.substr(0,2))<=26)
+            right = numDecod(dp,s.substr(2));
+       
+         dp[n] = left+right;
+        cout<<dp[n]<<endl;
+        return left+right;
+       
+    }
+    int numDecodings(string s) {
+        vector<int> dp(s.size()+1,-1);
+        dp[0]=0;
+        int t = numDecod(dp,s);
+        return dp[s.size()];
+       
+    }
+};
